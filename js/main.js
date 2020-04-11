d9f6ff7af82d0985b4fef324555910e9ab301209
Tibesti <51659129+Tibesti@users.noreply.github.com>
2020-04-10 19:57:37 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '100%';
+    document.getElementById('body-content').style.marginleft = '250px';
+}
+function closeSlideMenu() {
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('body-content').style.marginleft = '0';
+}
